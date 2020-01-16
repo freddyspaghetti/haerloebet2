1f69514624cbf82a70ac852dadd15cd2a930eeda
freddyspaghetti <frederik.bjorgensen@gmail.com>
2020-01-16 15:34:22 +0000

    Måske virker det nu?

--- javascript/tilmelding.js
+++ javascript/tilmelding.js
@@ -24,8 +24,15 @@
      console.log("Show information");
 
      document.querySelector(".overskrift").textContent = information.tilmelding_overskrift;
+     document.querySelector(".billede").textContent = information.billede;
      document.querySelector(".underoverskrift").textContent = information.tilmeld_her;
      document.querySelector(".link").href = information.tilmeldingslink;
      document.querySelector(".underoverskrift_2").textContent = information.praktisk_info;
-     document.querySelector(".txt").textContent = information.info_tilmelding;
+     document.querySelector(".liste").textContent = information.pris_punkt1;
+     document.querySelector(".liste").textContent = information.pris_punkt2;
+     document.querySelector(".liste").textContent = information.pris_punkt3;
+     document.querySelector(".txt").textContent = information.inkluderet_i_prisen;
+     document.querySelector(".liste").textContent = information.inkluderet_punkt1;
+     document.querySelector(".liste").textContent = information.inkluderet_punkt2;
+     document.querySelector(".liste").textContent = information.inkluderet_punkt3;
  }
